e28fc42d057ff08834d46c0ec341e6022da9ee9a
cherideal <36809060+cherideal@users.noreply.github.com>
2018-05-08 22:11:25 +0800

    Update GuessTheDict.js

--- GuessTheDict.js
+++ GuessTheDict.js
@@ -82,16 +82,121 @@ GuessTheDictContract.prototype = {
         this.bankVault.put(address, deposit);
     },
 
-    _save2dict: function (address, value) {
-        var orig_deposit = this.bankVault.get(address);
-        if (orig_deposit) {
-            value = value.plus(orig_deposit.balance);
+    _save2dict: function (address, value, point) {
+        var index = this.size;
+        var orig_info = this.dataMap.get(address);
+        if (orig_info) {
+            value = value.plus(orig_info.balance);
+        }
+        else {
+            this.arrayMap.set(index, address);
+            this.size += 1;
         }
 
-        var deposit = new DepositeContent();
-        deposit.balance = value;
+        var dict = new DictContent();
+        dict.balance = value;
+        dict.point = point;
+        dict.bonus = value;
 
-        this.bankVault.put(address, deposit);
+        this.dataMap.put(address, dict);
+    },
+
+    _stop: function () {
+        this.state = false;
+        console.log("Start to assign the award now!");
+        this._assign();
+        this._clear();
+    },
+
+    _clear: function () {
+        this.publisher = 0;
+        this.actorNumber = 0;
+        for (var i = 0; i < this.size; i++) {
+            var key = this.arrayMap.get(i);
+            this.arrayMap.del(i);
+            this.dataMap.del(key);
+        }
+        this.size = 0;
+    },
+
+    _takeout: function (address, value) {
+        var deposit = this.bankVault.get(address);
+        if (deposit){
+            deposit.balance = deposit.balance.sub(value);
+            this.bankVault.put(address, deposit);
+        }
+    },
+
+    _genPoint: function () {
+        BigNumber.config({CRYPTO: true})
+        var rand = BigNumber.random();
+        var point = rand.times(6).decimalPlaces(0, 1);
+        return point ? (point.integerValue() % 6) : 6;
+    },
+
+    _assign: function () {
+        var point = this._genPoint();
+        var accounts = [7];
+        for (var i = 0; i < 7; i++) {
+            accounts[i].amount = new BigNumber(0);
+            accounts[i].keys = [];
+        }
+
+        for (var i = 0; i < this.size; i++) {
+            var key = this.arrayMap.get(i);
+            var dict = this.dataMap.get(key);
+            accounts[dict.point].keys.append(key);
+            accounts[dict.point].amount = accounts[dict.point].amount.plus(dict.balance);
+        }
+
+        var winer = accounts[point];
+        for (var i = 1; i < 7; i++) {
+            if (i == point) {
+                continue;
+            }
+
+            for (var j = 0; j < accounts[i].keys.length; j++) {
+                var dict = this.dataMap.get(key);
+                if (winer.amount.gte(accounts[i].amount)) {
+                    dict.bonus = new BigNumber(0);
+                }
+                else {
+                    dict.bonus = (accounts[i].amount.minus(winer.amount)).times(dict.balance.div(accounts[i].amount));
+                }
+
+                this.dataMap.set(key, dict);
+            }
+        }
+
+        var rewardsTotal = new BigNumber(0);
+        for (var i = 0; i < this.size; i++) {
+            var key = this.arrayMap.get(i);
+            var dict = this.dataMap.get(key);
+            //winer's balance == bonus
+            rewardsTotal = rewardsTotal.plus(dict.balance.minus(dict.bonus));
+        }
+
+        var charges = rewardsTotal.multipliedBy(0.05);
+        var rewards = rewardsTotal.minus(charges);
+        for (var i = 0; i < winer.keys.length; i++) {
+            key = winer.keys[i];
+            var dict = this.dataMap.get(key);
+            dict.bonus = dict.bonus.plus(dict.balance.div(winer.amount).times(rewards));
+            this.dataMap.set(key, dict);
+        }
+
+        for (var i = 0; i < this.size; i++) {
+            var key = this.arrayMap.get(i);
+            var dict = this.dataMap.get(key);
+            var bonus = dict.bonus;
+            if (bonus.isZero()) {
+                continue;
+            }
+
+            this._save2bank(key, bonus);
+        }
+
+        this._save2bank(this.owner, charges);
     },
 
     save: function () {
@@ -154,32 +259,10 @@ GuessTheDictContract.prototype = {
             throw new Error("The game is stop already");
         }
 
-        if (this.publisher != Blockchain.transaction.from) {
-            throw new Error("Only publisher can stop the game manually");
-        }
-
         this.state = false;
         console.log("Start to assign the award now!");
         this._assign();
         this._clear();
-    },
-
-    _stop: function () {
-        this.state = false;
-        console.log("Start to assign the award now!");
-        this._assign();
-        this._clear();
-    },
-
-    _clear: function () {
-        this.publisher = 0;
-        this.actorNumber = 0;
-        for (var i = 0; i < this.size; i++) {
-            var key = this.arrayMap.get(i);
-            this.arrayMap.del(i);
-            this.dataMap.del(key);
-        }
-        this.size = 0;
     },
 
     action: function (point, funds) {
@@ -195,21 +278,12 @@ GuessTheDictContract.prototype = {
         var from = Blockchain.transaction.from;
         var value = Blockchain.transaction.value;
 
-        //funds >= value+orig_deposit
-        var orig_deposit = this.bankVault.get(from);
-        if (orig_deposit) {
-            value = value.plus(orig_deposit.balance);
-        }
+        this._save2bank(from, value);
 
-        if (value.lte(funds)){
-            throw new Error("Funds is not enough");
+        var deposit = this.bankVault.get(from);
+        if (deposit.lte(funds)){
+            funds = deposit.balance;
         }
-
-        this._move(funds);
-        value = value.minus(funds);
-        var deposit = new DepositeContent();
-        deposit.balance = value;
-        this.bankVault.put(from, deposit);
 
         if (point.isZero() || point.isNAN()) {
             point = 6;
@@ -218,121 +292,13 @@ GuessTheDictContract.prototype = {
             point = point.abs().mod(6);
         }
 
-        
-
-        var index = this.size;
-        var orig_info = this.dataMap.get(from);
-        if (orig_info) {
-            value = value.plus(orig_info.balance);
-        }
-        else {
-            this.arrayMap.set(index, from);
-            this.size += 1;
-        }
-
-        var dict = new DictContent();
-        dict.balance = value;
-        dict.point = point;
-        dict.bonus = value;
-
-        this.dataMap.put(from, dict);
+        //funds <= deposit
+        this._takeout(funds);
+        this._save2dict(from, funds, point);
 
         if (this.actorNumber >= this.size) {
             this._stop()
         }
-    },
-
-    _genPoint: function () {
-        BigNumber.config({CRYPTO: true})
-        var rand = BigNumber.random();
-        var point = rand.times(6).decimalPlaces(0, 1);
-        return point ? (point.integerValue() % 6) : 6;
-    },
-
-    _assign: function () {
-        var point = this._genPoint();
-        var accounts = [7];
-        for (var i = 0; i < 7; i++) {
-            accounts[i].amount = new BigNumber(0);
-            accounts[i].keys = [];
-        }
-
-        for (var i = 0; i < this.size; i++) {
-            var key = this.arrayMap.get(i);
-            var dict = this.dataMap.get(key);
-            accounts[dict.point].keys.append(key);
-            accounts[dict.point].amount = accounts[dict.point].amount.plus(dict.balance);
-        }
-
-        var winer = accounts[point];
-        for (var i = 1; i < 7; i++) {
-            if (i == point) {
-                continue;
-            }
-
-            for (var j = 0; j < accounts[i].keys.length; j++) {
-                var dict = this.dataMap.get(key);
-                if (winer.amount.isGreaterThanOrEqualTo(accounts[i].amount)) {
-                    dict.bonus = new BigNumber(0);
-                }
-                else {
-                    dict.bonus = (accounts[i].amount.minus(winer.amount)).times(dict.balance.div(accounts[i].amount));
-                }
-
-                this.dataMap.set(key, dict);
-            }
-        }
-
-        var rewardsTotal = new BigNumber(0);
-        for (var i = 0; i < this.size; i++) {
-            var key = this.arrayMap.get(i);
-            var dict = this.dataMap.get(key);
-            //winer's balance == bonus
-            rewardsTotal = rewardsTotal.plus(dict.balance.minus(dict.bonus));
-        }
-
-        var charges = rewardsTotal.multipliedBy(0.05);
-        var rewards = rewardsTotal.minus(charges);
-        for (var i = 0; i < winer.keys.length; i++) {
-            key = winer.keys[i];
-            var dict = this.dataMap.get(key);
-            dict.bonus = dict.bonus.plus(dict.balance.div(winer.amount).times(rewards));
-            this.dataMap.set(key, dict);
-        }
-
-        for (var i = 0; i < this.size; i++) {
-            var key = this.arrayMap.get(i);
-            var dict = this.dataMap.get(key);
-            var bonus = dict.bonus;
-            if (bonus.isZero()) {
-                continue;
-            }
-
-            var result = Blockchain.transfer(key, bonus);
-            if (!result) {
-                throw new Error("transfer failed.");
-            }
-
-            Event.Trigger("RewardDistribute", {
-                Transfer: {
-                    from: Blockchain.transaction.to,
-                    to: key,
-                    value: bonus.toString()
-                }
-            });
-        }
-
-        var result = Blockchain.transfer(this.owner, charges);
-        if (!result) {
-            throw new Error("transfer failed.");
-        }
-        Event.Trigger("CommissionDistribute", {
-            Transfer: {
-                from: Blockchain.transaction.to,
-                to: this.owner,
-                value: charges.toString()
-            }
-        });
     },
 
     balanceOf: function () {
